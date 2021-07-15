6fd8fb0e3862cd0ea67100589a80367f408110d4
Corkine <corkine@outlook.com>
2021-07-15 08:00:57 +0800

    init

---
+++ repo.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var port = flag.Int("port", 9001, "Http port")
+
+func main() {
+	flag.Parse()
+	help := func(w http.ResponseWriter, r *http.Request) {
+		data := map[string]string{
+			"version": "",
+			"log":     "",
+			"usage":   "import repo.mazhangjing.com/xxx to use https://gitee.com/corkine/xxx go mod repo",
+		}
+		bytes, err := json.Marshal(data)
+		_, err = w.Write(bytes)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+	http.HandleFunc("/about", help)
+	http.HandleFunc("/usage", help)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		uri := r.RequestURI
+		host := r.Host
+		goImport := fmt.Sprintf(
+			`<meta content="%s%s git https://gitee.com/corkine%s.git" name="go-import">`,
+			host, uri, uri)
+		newURL := fmt.Sprintf("https://gitee.com/corkine%s.git", uri)
+		htmlBody := fmt.Sprintf(`<!DOCTYPE html>
+			<html lang='zh-CN'>
+			<head>
+			<title>Go Simple Repo by CM</title>
+			<meta http-equiv="refresh" content="5;url=%s">
+			%s
+			</head>
+			<body>
+			<h1>Waiting for 5 seconds redirect to %s...</h1>
+			</body>
+			`, newURL, goImport, newURL)
+		_, err := w.Write([]byte(htmlBody))
+		if err != nil {
+			log.Println(err)
+		}
+	})
+	log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+}
---
+++ repo.sh
@@ -0,0 +1,17 @@
+export GOROOT=/usr/local/go
+export GO111MODULE=on
+
+cd /go-repo
+
+git pull
+
+kill $(ps axu | grep "/tmp/go.*/exe/repo.*" | grep -v grep | awk '{print $2}')
+nohup go run repo.go 1>>/var/log/go_repo.log 2>&1 &
+echo "goRepo run on Port \
+$(ps aux | grep "go run.*repo.go" | grep -v grep | awk '{print $2}')"
+
+echo "========================================================================="
+echo ""
+echo "server log here, press ctrl + c to exit (the server running normally) "
+echo ""
+echo "========================================================================="
